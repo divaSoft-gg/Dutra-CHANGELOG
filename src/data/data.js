c9fd5d8bb58221c40b91fe303b3a029ef79177b8
aymkh <aymen4real@gmail.com>
2024-07-15 01:31:02 +0100

    chore: initial commit

---
+++ src/data/data.js
@@ -0,0 +1,12 @@
+export const VERSION_DETAILS = [
+    {
+        "version": "1.0.0",
+        "date": "July 12, 2024",
+        "added": [
+            "User Mgmt: User authentication",
+            "User Mgmt: platform modules placeholders",
+        ],
+        "changed": [],
+        "fixed": [],
+    },
+];
