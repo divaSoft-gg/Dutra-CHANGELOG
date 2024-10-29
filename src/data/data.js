d00c5f2a4ddfbd0769ff050824ed0900c8a9838c
aymkh <aymen.khedhriya@gmail.com>
2024-10-29 17:31:08 +0100

    chore: version change

--- src/data/data.js
+++ src/data/data.js
@@ -1,10 +1,12 @@
 export const VERSION_DETAILS = [
     {
-        "version": "1.22.18",
-        "date": "WIP",
+        "version": "1.24.19",
+        "date": "October 29, 2024",
         "added": [
-           " Affaire Module: Added YouSign support",
-            "Affaire Module: Added affaire status"
+            "Affaire Module: Added YouSign support",
+            "Affaire Module: Added YouSign support",
+            "Clients Module: Added client status",
+            "User guide for the catalogue module",
         ],
         "changed": [
             "Affaire Module: Changed contract form for Appel d'offre",
